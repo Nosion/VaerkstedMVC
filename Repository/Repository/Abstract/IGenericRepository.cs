a6c70e31813f2c603f34ba7eb9c2323ba2e59ce4
Nicolai Steffensen <nicolai.steffensen@gmail.com>
2014-10-02 12:54:29 +0200

    Created project with template.

---
+++ Repository/Repository/Abstract/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+namespace Repository.Abstract
+{
+    public interface IGenericRepository<TEntity>
+    {
+        TEntity GetById(int? id);
+        IQueryable<TEntity> GetAll();
+        void Edit(TEntity entity);
+        void Insert(TEntity entity);
+        void Delete(TEntity entity);
+        void Dispose();
+    }
+}
